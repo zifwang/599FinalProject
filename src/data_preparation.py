e14849b0c03fadd7feccff570ca9e3f7b8ca811d
zifwang <zifwang@mymail.mines.edu>
2019-04-11 14:07:06 -0700

    get start with data_preparation.py

---
+++ src/data_preparation.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+
+
+
+def load_data_file(fileName):
+    """
+        This function is used to load data from file using pandas library
+        Arguments: fileName: The .csv file name
+        Returns:
+    """
+    
