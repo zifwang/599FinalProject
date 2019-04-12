5fd0268931939f85abeccfa40d1c1b33207f17fd
zifwang <zifwang@mymail.mines.edu>
2019-04-12 16:57:22 -0700

    get start with data_preparation.py

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -16,11 +16,17 @@ def load_data_file(fileName):
     df = df.drop(['Unnamed: 0','Unnamed: 36'], axis = 1)            # remove the first and the last column from the data frame
     indexed_df = df.loc[[0]]
     index = np.asarray(indexed_df)
-    pca = PCA(n_components = 34)
-    pca.fit(index)
-    print(pca.singular_values_)
-
-    # for i in df.index.values:
-    #     df.loc[[i]
+    print(index.shape)
+    # pca = PCA(n_components = 34)
+    # pca.fit(index)
+    # print(pca.singular_values_)
+    
+    # See the relationship in 24 hours 
+    flows_24 = []
+    for i in range (24):
+        index = np.asarray(df.loc[[i]])
+        flows_24.append(index)
+    flows_24_np = np.asarray(flows_24)
+    print(flows_24_np.shape)
 
 load_data_file('../data/TrainingFlow/flow_OD1_0-239.csv')
