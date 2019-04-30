146415893ff1ec46a62f64ce2cf2225ee1153dad
zifwang <zifwang@mymail.mines.edu>
2019-04-29 23:15:13 -0700

    finish flow_data_pca and OD_data_pca method

--- src/util.py
+++ src/util.py
@@ -10,12 +10,16 @@ def flow_data_pca(flows,numKernels):
             flows: input data with numpy type in the shape (number of data,features)
             numkernels: number of kernels used in PCA (constrained here make sure numKernels less than )
         Returns:
+            flows_reduced: reduce features in the flows
     """
     assert(numKernels <= flows.shape[1])            # Error: number of kernels are more than flows' features
     # Do PCA 
     pca = PCA(n_components = numKernels)
     pca.fit(flows)
-    pca.transform(flows)
+    print(pca.singular_values_)
+    print(pca.explained_variance_ratio_)
+    
+    return pca.transform(flows)
 
 def OD_data_pca(ODs,numKernels):
     """
@@ -24,5 +28,13 @@ def OD_data_pca(ODs,numKernels):
             ODs: input data with numpy type in the shape (number of data,features)
             numkernels: number of kernels used in PCA (constrained here make sure numKernels less than )
         Returns:
+            ODs_reduced: reduce features in the ori ODs
     """
+    assert(numKernels <= ODs.shape[1])              # Error: number of kernels are more than flows' features
+    # Do PCA
+    pca = PCA(n_components = numKernels)
+    pca.fit(ODs)
+    print(pca.singular_values_)
+    print(pca.explained_variance_ratio_)
     
+    return pca.transform(ODs)
