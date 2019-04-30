c03fcdf7abcd8d0ed8343733d6b72b61f8496353
zifwang <zifwang@mymail.mines.edu>
2019-04-30 01:59:49 -0700

    Finish training_data_generation() function

--- src/util.py
+++ src/util.py
@@ -3,7 +3,6 @@ import data_preparation
 """ Applying PCA function on flow vector """ 
 from sklearn.decomposition import PCA 
 
-
 def flow_data_pca(flows,numKernels):
     """
         This data_analysis function is used to analyze flows numpy array
@@ -40,16 +39,57 @@ def OD_data_pca(ODs,numKernels):
     
     return pca.transform(ODs)
 
-if __name__ == "__main__":
-    # Load File From Given Flow Directory
-    flows, flow_sequence = data_preparation.load_flow_directory_files('../data/TrainingFlow')
+def training_data_generation(flows_reduced,ODs_reduced,numDays):
+    """
+        Function to generate training data:
+            Reason: limit data-set
+        Conditions: Generate data in a four data period. Result data size: (numOfData,4(days)*24(hours),PCA_kernels)
+        Arguments:
+            flows_reduced: flows_data after pca
+            ODs_reduced: vectorized OD matrix data after pca
+            numDays: time length of RNN model
+        Returns:
+            X_train_shuffled: a shuffled flows_reduced data with size (numOfData,4(days)*24(hours),PCA_kernels)
+            Y_train_shuffled: a shuffled ODs_reduced data with size (numOfData,4(days)*24(hours),PCA_kernels)
+    """
+    # Define Constant:
+    HOURSINADAY = 24                                            # Define constant: total hour in a day
+    
+    assert(flows_reduced.shape[0] == ODs_reduced.shape[0])      # Number of Training Data does not match
 
-    # Load File From Given OD Directory
-    vectorized_od = data_preparation.load_OD_directiory_files('../data/TrainingOD',flow_sequence,flows)
+    # Data generation
+    flows_list = []
+    ODs_list = []
+    for i in range (0,flows_reduced.shape[0]-numDays*HOURSINADAY):
+        flows_list.append(flows_reduced[i:i+numDays*HOURSINADAY])
+        ODs_list.append(flows_reduced[i:i+numDays*HOURSINADAY])
+    # Change to numpy array
+    X_train = np.asarray(flows_list)
+    Y_train = np.asarray(ODs_list)
 
-    numKernels_flows = 10
-    _ = flow_data_pca(flows,numKernels_flows)
+    assert(X_train.shape[0] == Y_train.shape[0])                # Generate Data has different dimension
+    # Data shuffle
+    pi = np.random.permutation(X_train.shape[0])
+    X_train_shuffled = X_train[pi]
+    Y_train_shuffled = Y_train[pi]
+
+    return X_train_shuffled,Y_train_shuffled
 
 
-    numKernels_ODs = 10
-    _ = OD_data_pca(vectorized_od,numKernels_ODs)
+# if __name__ == "__main__":
+#     # Load File From Given Flow Directory
+#     flows, flow_sequence = data_preparation.load_flow_directory_files('../data/TrainingFlow')
+
+#     # Load File From Given OD Directory
+#     vectorized_od = data_preparation.load_OD_directiory_files('../data/TrainingOD',flow_sequence,flows)
+
+#     numKernels_flows = 15
+#     flows_reduced = flow_data_pca(flows,numKernels_flows)
+
+#     numKernels_ODs = 15
+#     ODs_reduced = OD_data_pca(vectorized_od,numKernels_ODs)
+
+#     X_train,Y_train = training_data_generation(flows_reduced,ODs_reduced,4)
+
+#     print(X_train.shape)
+#     print(Y_train.shape)
